a1a9acd6849e136528bd0dc2d5e341de3ffab065
miraj <miraj00@gmail.com>
2023-02-02 12:17:41 -0500

    code changes made to run program

---
+++ src/main/java/co/springmvcpizzalab/controller/pizzaController.java
@@ -0,0 +1,13 @@
+package co.springmvcpizzalab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class pizzaController {
+
+	@RequestMapping("/")
+	public String homepage() {
+		return "index";
+	}
+}
